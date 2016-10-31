bb26791f551b15f7e225a4dab35819c93eb3f85d
Julius Kriukas <julius@kriukas.lt>
2016-10-31 11:30:09 +0200

    Remove vultr

--- cli_handlers.go
+++ cli_handlers.go
@@ -30,7 +30,6 @@ import (
 	"github.com/xenolf/lego/providers/dns/pdns"
 	"github.com/xenolf/lego/providers/dns/rfc2136"
 	"github.com/xenolf/lego/providers/dns/route53"
-	"github.com/xenolf/lego/providers/dns/vultr"
 	"github.com/xenolf/lego/providers/http/memcached"
 	"github.com/xenolf/lego/providers/http/webroot"
 )
@@ -158,8 +157,6 @@ func setup(c *cli.Context) (*Configuration, *Account, *acme.Client) {
 			provider, err = route53.NewDNSProvider()
 		case "rfc2136":
 			provider, err = rfc2136.NewDNSProvider()
-		case "vultr":
-			provider, err = vultr.NewDNSProvider()
 		case "ovh":
 			provider, err = ovh.NewDNSProvider()
 		case "pdns":
